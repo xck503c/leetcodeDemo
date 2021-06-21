0940aba9f0b1c0a122c174727119b0c6f74a4ec3
xck503c <2198494838@qq.com>
2021-06-21 21:27:04 +0800

    Initial commit

---
+++ src/main/java/com/xck/y2021/TowNumAdd.java
@@ -0,0 +1,44 @@
+package com.xck.y2021;
+
+/**
+ * @Classname TowNumAdd
+ *
+ * leetcode 上两数之和：https://leetcode-cn.com/problems/two-sum
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ *
+ * 示例2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ *
+ * 示例3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *
+ * @Description TODO
+ * @Date 2021/6/21 21:23
+ * @Created by xck503c
+ */
+public class TowNumAdd {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 解法1：双指针法，先将数组进行排序，然后
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum1(int[] nums, int target) {
+        
+    }
+}

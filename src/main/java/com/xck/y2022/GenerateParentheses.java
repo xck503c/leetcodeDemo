4701bcc9f848c09e1bd3c1e661999ef603c7a07a
xck503c <2198494838@qq.com>
2022-02-19 17:03:53 +0800

    feat: 括号生成，动态规划和手动栈实现

--- src/main/java/com/xck/y2022/GenerateParentheses.java
+++ src/main/java/com/xck/y2022/GenerateParentheses.java
@@ -1,10 +1,11 @@
 package com.xck.y2022;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 括号生成
+ *
+ * 动态规划，回溯法，bfs，理解手动栈和系统栈区别
  *
  * @author xuchengkun
  * @date 2022/02/18 10:33
@@ -14,6 +15,15 @@ public class GenerateParentheses {
     public static void main(String[] args) {
         System.out.println(generateParenthesisDFS1(3));
         System.out.println(generateParenthesisDFS1(4));
+
+        System.out.println(generateParenthesisStack1(3));
+        System.out.println(generateParenthesisStack1(4));
+
+        System.out.println(generateParenthesisStack2(3));
+        System.out.println(generateParenthesisStack2(4));
+
+        System.out.println(generateParenthesisDp(3));
+        System.out.println(generateParenthesisDp(4));
     }
 
     public static List<String> generateParenthesisDFS1(int n) {
@@ -46,6 +56,7 @@ public class GenerateParentheses {
             return;
         }
 
+        //这里如果不计算会更快
         if (dfsResult.singleBracket < dfsResult.n - dfsResult.pairBracket) {
             dfsResult.bracketStr.append("(");
             dfsResult.singleBracket++;
@@ -74,7 +85,8 @@ public class GenerateParentheses {
         StringBuilder bracketStr = new StringBuilder();
     }
 
-    public static List<String> generateParenthesisDFS2(int n) {
+    //显示回溯
+    public static List<String> generateParenthesisStack1(int n) {
         List<String> result = new ArrayList<>();
 
         if (n == 1) {
@@ -92,6 +104,8 @@ public class GenerateParentheses {
         nodes[0] = new Node(1, 0, false);
         int top = 1;
 
+        boolean isBack = false; //是否处于回溯状态
+
         //栈不为空
         while (top != 0) {
             //若栈满，这表示已经得到了结果
@@ -101,27 +115,134 @@ public class GenerateParentheses {
                     sb.append(node.isRight ? ")" : "(");
                 }
                 result.add(sb.toString());
-                //弹出栈顶
-                Node topNode = nodes[--top];
-//                if (topNode.)
+                isBack = true;
             }
 
-
+            if (isBack) {
+                //因为顺序是先左后右，所以如果top是右括号则表示要继续退栈
+                --top;
+                while (top > 0 && nodes[top].isRight) {
+                    --top;
+                }
+                if (top <= 0) {
+                    break;
+                }
+                Node nextNode = nodes[top - 1];
+                //保证之前试过必须是左括号，现在试右括号
+                if (nextNode.openBracket > nextNode.closeBracket) {
+                    nodes[top++] = new Node(nextNode.openBracket
+                            , nextNode.closeBracket + 1, true);
+                    isBack = false;
+                }
+            } else {
+                Node nextNode = nodes[top - 1];
+                if (nextNode.openBracket < n) {
+                    nodes[top++] = new Node(nextNode.openBracket + 1
+                            , nextNode.closeBracket, false);
+                    isBack = false;
+                } else if (nextNode.openBracket > nextNode.closeBracket) {
+                    nodes[top++] = new Node(nextNode.openBracket
+                            , nextNode.closeBracket + 1, true);
+                    isBack = false;
+                } else {
+                    isBack = true;
+                }
+            }
         }
 
         return result;
     }
 
-    public static class Node{
+    //隐式回溯
+    public static List<String> generateParenthesisStack2(int n) {
+        List<String> result = new ArrayList<>();
+
+        if (n == 1) {
+            result.add("()");
+            return result;
+        }
+
+        if (n == 2) {
+            result.add("()()");
+            result.add("(())");
+            return result;
+        }
+
+        List<String> res = new ArrayList<>();
+        if (n == 0) {
+            return res;
+        }
+        Stack<Node> queue = new Stack<>();
+        queue.push(new Node("", n, n));
+
+        while (!queue.isEmpty()) {
+
+            //每次选取最上面的一个进行深度遍历
+            Node curNode = queue.pop();
+            if (curNode.openBracket == 0 && curNode.closeBracket == 0) {
+                res.add(curNode.res);
+            }
+            //将所有分支进行压栈
+            if (curNode.openBracket > 0) {
+                queue.push(new Node(curNode.res + "(", curNode.openBracket - 1
+                        , curNode.closeBracket));
+            }
+            if (curNode.closeBracket > 0 && curNode.openBracket < curNode.closeBracket) {
+                queue.push(new Node(curNode.res + ")", curNode.openBracket
+                        , curNode.closeBracket - 1));
+            }
+        }
+        return res;
+    }
+
+    public static class Node {
         private int openBracket;
         private int closeBracket;
-
+        private String res;
         private boolean isRight; //是否是右括号
+
+        public Node(String res, int openBracket, int closeBracket) {
+            this.openBracket = openBracket;
+            this.closeBracket = closeBracket;
+            this.res = res;
+        }
 
         public Node(int openBracket, int closeBracket, boolean isRight) {
             this.openBracket = openBracket;
             this.closeBracket = closeBracket;
             this.isRight = isRight;
         }
+    }
+
+    static List<String>[] cache = new ArrayList[9];
+
+    static {
+        cache[0] = new ArrayList<>(Arrays.asList(new String[]{""}));
+        cache[1] = new ArrayList<>(Arrays.asList(new String[]{"()"}));
+        cache[2] = new ArrayList<>(Arrays.asList(new String[]{"()()", "(())"}));
+    }
+
+    public static List<String> generateParenthesisDp(int n) {
+        if (cache[n] != null) return cache[n];
+
+        for (int i = 3; i <= n; i++) {
+            //若已经有缓存则跳过
+            if (cache[i] != null) continue;
+            List<String> result = new ArrayList<>();
+            //根据(a)b的序列进行自底向上求解
+            for (int a = 0, b = n - 1; a < n; a++, b--) {
+                List<String> aList = cache[a];
+                List<String> bList = cache[b];
+                for (String aItem : aList) {
+                    for (String bItem : bList) {
+                        result.add("(" + aItem + ")" + bItem);
+                    }
+                }
+
+            }
+            cache[i] = result;
+        }
+
+        return cache[n];
     }
 }
